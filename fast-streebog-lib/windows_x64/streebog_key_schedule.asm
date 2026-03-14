ea21abb2960d7cebe15b28e510234ba41c3957b5
cKPPPPPP <ladokhin.mega@gmail.com>
2026-03-14 13:16:13 +0300

    Пункт А

--- fast-streebog-lib/windows_x64/streebog_key_schedule.asm
+++ fast-streebog-lib/windows_x64/streebog_key_schedule.asm
@@ -1,5 +1,6 @@
 .data
-ALIGN 16
+ALIGN 64
+
 C_0:
     DB 0b1h, 008h, 05bh, 0dah, 01eh, 0cah, 0dah, 0e9h, 0ebh, 0cbh, 02fh
     DB 081h, 0c0h, 065h, 07ch, 01fh, 02fh, 06ah, 076h, 043h, 02eh, 045h
@@ -96,7 +97,6 @@ C_11:
     DB 088h, 0e1h, 028h, 052h, 0fah, 0f4h, 017h, 0d5h, 0d9h, 0b2h, 01bh
     DB 099h, 048h, 0bch, 092h, 04ah, 0f1h, 01bh, 0d7h, 020h
 
-
 C_TABLE:
     QWORD C_0
     QWORD C_1
@@ -112,53 +112,53 @@ C_TABLE:
     QWORD C_11
 
 .code
-
-
-EXTERN streebog_xor_512:PROC
 EXTERN streebog_s_transform:PROC
 EXTERN streebog_p_transform:PROC
 EXTERN streebog_l_transform:PROC
 
 streebog_key_schedule PROC
-    
+
     push rbx
     push rsi
     push r12
     push r13
-    
-    sub rsp, 40         
+    sub  rsp, 40            
 
-    mov rsi, rcx        
-    mov r12d, edx       
-    mov r13, r8         
-    
-    
-    lea rbx, [C_TABLE]
+    mov rsi,  rcx
+    mov r12d, edx           
+    mov r13,  r8            
+
+    lea  rbx, [C_TABLE]
     movsxd rax, r12d
-    mov rbx, QWORD PTR [rbx + rax*8]  
-    
-    mov rcx, rsi        
-    mov rdx, rbx        
-    mov r8, r13         
-    call streebog_xor_512
-    
-    mov rcx, r13        
-    mov rdx, r13        
+    mov  rbx, QWORD PTR [rbx + rax*8]  
+
+    vmovdqu64 zmm0, ZMMWORD PTR [rsi]   
+    vmovdqu64 zmm1, ZMMWORD PTR [rbx]   
+
+    vpxorq zmm0, zmm0, zmm1 
+
+    vmovdqu64 ZMMWORD PTR [r13], zmm0 
+
+    mov rcx, r13
+    mov rdx, r13
     call streebog_s_transform
-    
-    mov rcx, r13        
-    mov rdx, r13        
+
+    mov rcx, r13
+    mov rdx, r13
     call streebog_p_transform
-    
-    mov rcx, r13        
-    mov rdx, r13        
+
+    mov rcx, r13
+    mov rdx, r13
     call streebog_l_transform
-    
-    add rsp, 40
-    pop r13
-    pop r12
-    pop rsi
-    pop rbx
+
+    vzeroupper
+
+    add  rsp, 40
+    pop  r13
+    pop  r12
+    pop  rsi
+    pop  rbx
     ret
+
 streebog_key_schedule ENDP
 END
--- fast-streebog-lib/windows_x64/streebog_l_transform.asm
+++ fast-streebog-lib/windows_x64/streebog_l_transform.asm
@@ -1,86 +1,78 @@
-; Optimized Streebog L-transform using precalculated lookup tables
-; This version uses table lookups instead of bit-by-bit operations
-; Expected speedup: 8-35x faster than the original implementation
-
 .data
-ALIGN 16
+ALIGN 64 ; выравние до 64 изза avx-512
 
-; Include precalculated Ax tables
+CONST_FF dq 8 DUP (000000000000000FFh) ; константа для доставания элементов, 8 раз по FF на конце
+
 INCLUDE streebog_precalc_tables.asm
 
 .code
 
 streebog_l_transform PROC
-    ; rcx = input pointer (const uint8_t *state)
-    ; rdx = output pointer (uint8_t *out)
-    
-    push rbx
-    push rsi
-    push rdi
-    push r12
-    push r13
-    push r14
-    push r15
-    
-    mov rsi, rcx        ; rsi = input
-    mov rdi, rdx        ; rdi = output
-    
-    ; Process 8 blocks of 8 bytes each
-    xor r12, r12        ; r12 = block counter (0-7)
-    
-process_block:
-    ; Load 8 bytes from input block
-    movzx rax, BYTE PTR [rsi + r12*8 + 0]   ; byte 0
-    movzx rbx, BYTE PTR [rsi + r12*8 + 1]   ; byte 1
-    movzx rcx, BYTE PTR [rsi + r12*8 + 2]   ; byte 2
-    movzx r8,  BYTE PTR [rsi + r12*8 + 3]   ; byte 3
-    movzx r9,  BYTE PTR [rsi + r12*8 + 4]   ; byte 4
-    movzx r10, BYTE PTR [rsi + r12*8 + 5]   ; byte 5
-    movzx r11, BYTE PTR [rsi + r12*8 + 6]   ; byte 6
-    movzx r13, BYTE PTR [rsi + r12*8 + 7]   ; byte 7
-    
-    ; Lookup in tables: result = Ax[0][byte0] ^ Ax[1][byte1] ^ ... ^ Ax[7][byte7]
-    lea r14, [Ax_COL0]
-    mov r15, QWORD PTR [r14 + rax*8]        ; r15 = Ax[0][byte0]
-    
-    lea r14, [Ax_COL1]
-    xor r15, QWORD PTR [r14 + rbx*8]        ; r15 ^= Ax[1][byte1]
-    
-    lea r14, [Ax_COL2]
-    xor r15, QWORD PTR [r14 + rcx*8]        ; r15 ^= Ax[2][byte2]
-    
-    lea r14, [Ax_COL3]
-    xor r15, QWORD PTR [r14 + r8*8]         ; r15 ^= Ax[3][byte3]
-    
-    lea r14, [Ax_COL4]
-    xor r15, QWORD PTR [r14 + r9*8]         ; r15 ^= Ax[4][byte4]
-    
-    lea r14, [Ax_COL5]
-    xor r15, QWORD PTR [r14 + r10*8]        ; r15 ^= Ax[5][byte5]
-    
-    lea r14, [Ax_COL6]
-    xor r15, QWORD PTR [r14 + r11*8]        ; r15 ^= Ax[6][byte6]
-    
-    lea r14, [Ax_COL7]
-    xor r15, QWORD PTR [r14 + r13*8]        ; r15 ^= Ax[7][byte7]
-    
-    ; Convert to big-endian and store
-    bswap r15
-    mov QWORD PTR [rdi + r12*8], r15
-    
-    ; Next block
-    inc r12
-    cmp r12, 8
-    jl process_block
-    
-    pop r15
-    pop r14
-    pop r13
-    pop r12
-    pop rdi
-    pop rsi
-    pop rbx
+
+    vmovdqa64 zmm8,  ZMMWORD PTR [rcx]          ; вгруз входных данных за раз
+    vmovdqa64 zmm10, ZMMWORD PTR [CONST_FF]     ; загрузка константы, которой мы будем манипулировать
+
+    ; каждый нулевой байт из всех колонн
+    vpandq    zmm9,  zmm8,  zmm10               ; в 9 змм грузим нулевые байты
+    vmovdqa64 zmm0,  ZMMWORD PTR [Ax_COL0]
+    vpermb    zmm16, zmm9,  zmm0                ; вгрузв в 16 змм нулевого байта всех значений
+
+    ; первый байт
+    vpsrlq    zmm9,  zmm8,  8                   ; сдвиг вправо одного байта
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm1,  ZMMWORD PTR [Ax_COL1]
+    vpermb    zmm17, zmm9,  zmm1
+
+    ; второй байт
+    vpsrlq    zmm9,  zmm8,  16
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm2,  ZMMWORD PTR [Ax_COL2]
+    vpermb    zmm18, zmm9,  zmm2
+
+    ; третий байт
+    vpsrlq    zmm9,  zmm8,  24
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm3,  ZMMWORD PTR [Ax_COL3]
+    vpermb    zmm19, zmm9,  zmm3
+
+    ; четвертый байт
+    vpsrlq    zmm9,  zmm8,  32
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm4,  ZMMWORD PTR [Ax_COL4]
+    vpermb    zmm20, zmm9,  zmm4
+
+    ; пятый байт
+    vpsrlq    zmm9,  zmm8,  40
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm5,  ZMMWORD PTR [Ax_COL5]
+    vpermb    zmm21, zmm9,  zmm5
+
+    ; шестой байт
+    vpsrlq    zmm9,  zmm8,  48
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm6,  ZMMWORD PTR [Ax_COL6]
+    vpermb    zmm22, zmm9,  zmm6
+
+    ; седьмой байт
+    vpsrlq    zmm9,  zmm8,  56
+    vpandq    zmm9,  zmm9,  zmm10
+    vmovdqa64 zmm7,  ZMMWORD PTR [Ax_COL7]
+    vpermb    zmm23, zmm9,  zmm7
+
+    ; ксор всех элементов, происходят параллельно
+    vpxorq    zmm16, zmm16, zmm17
+    vpxorq    zmm18, zmm18, zmm19
+    vpxorq    zmm20, zmm20, zmm21
+    vpxorq    zmm22, zmm22, zmm23
+
+    vpxorq    zmm16, zmm16, zmm18
+    vpxorq    zmm20, zmm20, zmm22
+
+    vpxorq    zmm24, zmm16, zmm20
+
+    vmovdqa64 ZMMWORD PTR [rdx], zmm24 ; загружаем в ответ
+    vzeroupper
+
     ret
 streebog_l_transform ENDP
-
 END
